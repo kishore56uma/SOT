c36aff0c3aef9f616912c62df300f3e370f32bc8
kishore56uma <133082982+kishore56uma@users.noreply.github.com>
2023-06-21 17:45:05 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+function Addition(){
+    var x= Number(document.getElementById("fnum").value);
+    var y =Number(document.getElementById("snum").value);
+    sum = x + y;
+    alert(sum);
+}
+ 
+ 
